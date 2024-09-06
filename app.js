30ee0a9aa984c3f6152a5e6a0c8b98ebecb3e1aa
luvitix <happymp4255@gmail.com>
2024-09-07 04:20:36 +0900

    error

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
-app.use(express.static(path.join('public')));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/:id1', (req, res) => {
     const id1 = req.params.id1;
-    let dynamicHtmlPath = ""
+    let dynamicHtmlPath = "";
 
-    // 어떤 페이지로 갈 지 결정
-    if (id1 == "objekt-view") {
-        dynamicHtmlPath = path.join('public', 'objekt-view.html');
+    // 어떤 페이지로 갈지 결정
+    if (id1 == "funding") {
+        dynamicHtmlPath = path.join(__dirname, 'public', 'funding.html');
+    } else if (id1 == "store") {
+        dynamicHtmlPath = path.join(__dirname, 'public', 'store.html');
     } else {
-        dynamicHtmlPath = path.join('public', 'server.html');
+        dynamicHtmlPath = path.join(__dirname, 'public', 'server.html');
     }
 
     try {
         // 파일을 동기적으로 읽어옴
-        let data = fs.readFileSync(dynamicHtmlPath, 'utf8')
+        let data = fs.readFileSync(dynamicHtmlPath, 'utf8');
         console.log(data); // 로깅 추가
         // id1을 직접 삽입
         const dynamicHtml = data.replace(/{{objektId}}/g, id1);
