ce69b2d280aaa0dbf70363022745b8e5a332d397
luvitix <happymp4255@gmail.com>
2024-12-28 19:34:23 +0900

    community fix

--- public/community.js
+++ public/community.js
@@ -210,10 +210,16 @@ extraInput.addEventListener("change", handleFileSelection);
 
 // 이미지 제거 함수
 async function removeImage(index) {
-  try {
-    selectedFiles.splice(index, 1); // 배열에서 제거
-  } finally {
-    await renderPreviews();
+  if (img_handle == true) {
+    try {
+      img_handle == false;
+      selectedFiles.splice(index, 1); // 배열에서 제거
+    } finally {
+      img_handle == true;
+      await renderPreviews();
+    }
+  } else {
+
   }
 }
 
@@ -261,19 +267,18 @@ async function renderPreviews() {
 // 파일 선택 이벤트 처리 함수
 async function handleFileSelection(event) {
     const files = event.target.files;
-  
+
+    try {
     // 현재 파일 개수와 새로 추가될 파일 개수 확인
     if (selectedFiles.length + files.length > MAX_FILES) {
       alert(`최대 ${MAX_FILES}개 파일만 업로드할 수 있습니다.`);
     } else {
-      try {
-        // 파일 추가
-        Array.from(files).forEach((file) => {
-          selectedFiles.push(file);
-        });
-      } finally {
-        await renderPreviews();
-      }
+      Array.from(files).forEach((file) => {
+        selectedFiles.push(file);
+      });
+    }
+    } finally {
+      await renderPreviews();
     }
 }
   
